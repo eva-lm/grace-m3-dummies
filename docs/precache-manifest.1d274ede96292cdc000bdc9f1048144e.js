33146db0aa9bb0e1589c07f1ef079fb9f0a7ca75
btdvalle <btdvalle@gmail.com>
2019-09-16 20:50:45 +0200

    Update docs

---
+++ docs/precache-manifest.1d274ede96292cdc000bdc9f1048144e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8cbf1435af671e73f57f13339df886",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ddad571cbf75892bf67",
+    "url": "./static/css/main.299d3b2a.chunk.css"
+  },
+  {
+    "revision": "566b21fe445aa7bd8c06",
+    "url": "./static/js/2.4fb187e4.chunk.js"
+  },
+  {
+    "revision": "6ddad571cbf75892bf67",
+    "url": "./static/js/main.0113b11e.chunk.js"
+  },
+  {
+    "revision": "697bde2c2a0eb852c77e",
+    "url": "./static/js/runtime~main.b8dc1b6c.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/awesome.0118e311.svg"
+  },
+  {
+    "revision": "dfe10ed9a22a686a0ceb70c97c70d667",
+    "url": "./static/media/natalie-portman.dfe10ed9.jpg"
+  }
+]);
