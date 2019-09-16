70f85bbc6800b363d4815b795172bc0ec79e9826
btdvalle <btdvalle@gmail.com>
2019-09-16 11:22:39 +0200

    Add saveData function

--- src/components/App.js
+++ src/components/App.js
@@ -27,32 +27,43 @@ class App extends React.Component {
     this.handlePaletteApp = this.handlePaletteApp.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handlePhotoApp = this.handlePhotoApp.bind(this);
+    this.saveData = this.saveData.bind(this);
     this.fillIconInputs();
   }
   // Change color
   handlePaletteApp(palette) {
+    //PASAN STRANGER THINGS CON LAS PALETAS EN EL LOCAL STORAGE
     const newUserInfo = { ...this.state.userInfo, palette: palette };
-    this.setState({ userInfo: newUserInfo }, console.log(this.state));
+    this.setState({ userInfo: newUserInfo }, this.saveData(this.state.userInfo));
   }
   // Write input values on card
   handleInputChange(event) {
     const inputValue = event.currentTarget.value;
     const id = event.currentTarget.id;
     const newUserInfo = { ...this.state.userInfo, [id]: inputValue };
-    this.setState({ userInfo: newUserInfo }, console.log(this.fillIconInputs(id)));
+    this.setState(
+      { userInfo: newUserInfo },
+      //  this.fillIconInputs(id),
+      this.saveData(this.state.userInfo)
+    );
   }
 
   // Change profile picture
   handlePhotoApp(photo) {
     console.log(photo);
     const newUserInfo = { ...this.state.userInfo, photo: photo };
-    this.setState({ userInfo: newUserInfo }, console.log(this.state));
+    this.setState({ userInfo: newUserInfo }, this.saveData(this.state.userInfo));
   }
 
   //Opacity card icons
   fillIconInputs(id) {
     //no funciona por la movida esa de la asincronia. hay que hacer esta funcion como un callback
     return !!this.state.userInfo[id] ? "" : "clear";
+  }
+
+  //LocalStorage
+  saveData(userInfo) {
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
   }
 
   render() {
