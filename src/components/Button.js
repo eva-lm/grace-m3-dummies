f483ddc0f7b2f6993954fc293ff9809378506b46
isabel pizarro <chabelipizarro1@gmail.com>
2019-09-20 12:33:59 +0200

    addproptypes buttoncomponent

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Button = props => <div className={props.className}></div>;
 export default Button;
+
+Button.propTypes = {
+  className: PropTypes.string
+};
