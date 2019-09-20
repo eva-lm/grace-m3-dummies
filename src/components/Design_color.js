770f9e3e352b60ca543db779ff74b3cfc03eac03
btdvalle <btdvalle@gmail.com>
2019-09-20 13:41:01 +0200

    Fix checked

--- src/components/Design_color.js
+++ src/components/Design_color.js
@@ -7,30 +7,18 @@ class ColorPalette extends React.Component {
     this.handlePalette = this.handlePalette.bind(this);
   }
 
-  checkPalette(palette) {
-    if (this.props.currentValue === this.props.id) {
-      palette.checked;
-    }
+  checkPalette() {
+    return this.props.currentValue === this.props.id ? true : false;
   }
 
   handlePalette(ev) {
     this.props.handlePaletteDesignColors(this.props.id);
-    const palette = ev.currentTarget;
-
-    //reconocer el input de ese id y ponerle.checked;
   }
   render() {
-    console.log(this.props.currentValue, this.props.id);
+    // console.log(this.props.currentValue, this.props.id);
     return (
       <label htmlFor={this.props.for} className="color__box">
-        <input
-          type="radio"
-          htmlFor={this.props.for}
-          className="radio_btn"
-          name="design"
-          value={this.props.value}
-          onClick={this.handlePalette}
-        />
+        <input type="radio" htmlFor={this.props.for} className="radio_btn" name="design" value={this.props.value} onClick={this.handlePalette} checked={this.checkPalette()} />
         <div className={`color__one--${this.props.number}`}></div>
         <div className={`color__two--${this.props.number}`}></div>
         <div className={`color__three--${this.props.number}`}></div>
