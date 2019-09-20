a200c2beca67d3d48b82e270988fc0227dc55257
raquelmontilla <rmontillacalvo@gmail>
2019-09-20 20:30:56 +0200

    animation button ok

--- src/components/Share.js
+++ src/components/Share.js
@@ -10,12 +10,12 @@ class Share extends React.Component {
   }
 
   showApiResult = () => {
-    debugger;
     if (this.props.cardURL) {
       return (
         <div>
           <p className="share__result">
-            <a className="share__result-link bttn" href={this.props.cardURL} target="_blank" >{this.props.cardURL}</a>
+            <a
+              className="share__result-link" href={this.props.cardURL} target="_blank" >{this.props.cardURL}</a>
             <a href={`https://twitter.com/intent/tweet?text=Created+with+Awesome+Profile+Cards+&url=${this.props.cardURL}`} className="twitterLink js-twitterLink" target="_blank">
               <div className="share__button--blue">
                 <i className="fab fa-twitter share__button__icon"></i>
@@ -48,7 +48,7 @@ class Share extends React.Component {
     return (
       <div className="folded__form js-form">
         <div className="share__button">
-          <button type="submit" className="share__button--orange bttn js-shareBtn" onClick={this.props.getDataFromApi}>
+          <button type="submit" className="share__button--orange js-shareBtn" onClick={this.props.getDataFromApi}>
             <i className="far fa-address-card share__button--orange--icon"></i>
             <h2 className="share__button--orange--text">CREAR TARJETA</h2>
           </button>
